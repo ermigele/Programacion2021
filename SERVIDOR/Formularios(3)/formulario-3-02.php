80a457668ee029118d25f6b61a1eb4355fbf0737
ermigele <ermigele@gmail.com>
2020-11-11 14:53:47 +0100

    11/11 14.53

--- SERVIDOR/Formularios(3)/formulario-3-02.php
+++ SERVIDOR/Formularios(3)/formulario-3-02.php
@@ -7,19 +7,20 @@
 </head>
 
 <body>
-    <h1>Calculadora de divisiones</h1>
-    <form action="" method="GET" />
-        <label>Dividendo:</label> <input type="number" name="dividendo"> <br>
-        <label>Divisor:</label> <input type="number" name="divisor"> <br>
-        <input type="submit" value="Calcular">
-    </form>
 
     <?php
-    if (!isset($_REQUEST['dividendo']) && !isset($_REQUEST['divisor'])) {
-        $d1 = 0;
-        $d2 = 0;
-    } else {
+    if (!isset($_REQUEST['dividendo']) && empty($_REQUEST['divisor'])) {
+    ?>
+        <h1>Calculadora de divisiones</h1>
+        <form action="" method="GET">
+            <label>Dividendo:</label> <input type="number" name="dividendo"> <br>
+            <label>Divisor:</label> <input type="number" name="divisor"> <br>
+            <input type="submit" value="Calcular">
+        </form>
 
+    <?php
+    } else {
+        print("<h2>Resultado</h2>");
         $d1 = $_REQUEST['dividendo'];
         $d2 = $_REQUEST['divisor'];
         if ((!is_numeric($d1) && !is_numeric($d2)) || ($d1 >= 10000 && $d2 >= 10000)) {
@@ -29,7 +30,7 @@
         } else {
             $conciente = floor($d1 / $d2);
             $resto = $d1 - $conciente * $d2;
-            echo("<br/>Dividendo: ".$d1 . "<br/> Divisor: " . $d2 . "<br/>Conciente: " . $conciente . "<br/>Resto: " . $resto);
+            echo ("<br/>Dividendo: " . $d1 . "<br/> Divisor: " . $d2 . "<br/>Conciente: " . $conciente . "<br/>Resto: " . $resto);
         }
     }
     ?>
