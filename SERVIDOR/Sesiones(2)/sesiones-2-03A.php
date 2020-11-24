6ecf4f816ce159ae6519f5905b24dc364efc7595
ermigele <ermigele@gmail.com>
2020-11-24 08:39:12 +0100

    24/11 8.39

--- SERVIDOR/Sesiones(2)/sesiones-2-03A.php
+++ SERVIDOR/Sesiones(2)/sesiones-2-03A.php
@@ -1,12 +1,14 @@
 <?php
 session_start();
-if (!isset($_SESSION["tragaperras"])) {
+if (!isset($_SESSION["tragaperras"]) || !isset($_SESSION["frutas"])) {
     $_SESSION["tragaperras"] = [0, 0];
+    $_SESSION["frutas"] = [];
 }
 //session_destroy();
 $_SESSION["tragaperras"][0];
 $_SESSION["tragaperras"][1];
-$frutas = [];
+$_SESSION["frutas"];
+$FRESA = 127827;
 ?>
 
 <!DOCTYPE html>
@@ -22,25 +24,68 @@ $frutas = [];
     <table border=1,5>
         <tr>
             <?php
-            $cont = 1;
-            $frutas = [];
-            for ($i = 0; $i <= 2; $i++) {
-                $frutaAleatoria = rand(127817, 127827);
-                print("<td> <p style=\"font-size: 600%; margin: 0\"> &#$frutaAleatoria </p> </td>");
-
-                if (isset($_REQUEST['opc']) && $_REQUEST['opc'] == 'Jugar') {
-                    if (in_array($frutaAleatoria, $frutas)) {
-                        $cont += 1;
-                    }
+            if (!isset($_REQUEST['opc'])) {
+                for ($i = 0; $i <= 2; $i++) {
+                    $frutaAleatoria = rand(127817, 127827);
+                    print("<td> <p style=\"font-size: 600%; margin: 0\"> &#$frutaAleatoria </p> </td>");
+                    $frutas[] = $frutaAleatoria;
                 }
-                $frutas[] = $frutaAleatoria;
-            }
+                $_SESSION["frutas"] = $frutas;
+            } else if ($_REQUEST['opc'] == 'Jugar') {
+                $contIgual = 0;
+                $contFresa = 0;
+                $frutas = [];
+                for ($i = 0; $i <= 2; $i++) {
+                    $frutaAleatoria = rand(127817, 127827);
+                    print("<td> <p style=\"font-size: 600%; margin: 0\"> &#$frutaAleatoria </p> </td>");
 
-            if ($cont > 2) {
-                $_SESSION["tragaperras"][0] += 10;
-                $_SESSION["tragaperras"][1] += 1;
+                    if ($frutaAleatoria == $FRESA) {
+                        $contFresa += 1;
+                    } else if (in_array($frutaAleatoria, $frutas)) {
+                        $contIgual += 1;
+                    }
+                    $frutas[] = $frutaAleatoria;
+                }
+                $_SESSION["frutas"] = $frutas;
+
+                switch ($contFresa) {
+                    case 1:
+                        if ($contIgual > 1) {
+                            $_SESSION["tragaperras"][0] += 3;
+                            $_SESSION["tragaperras"][1] += 1;
+                        } else {
+                            $_SESSION["tragaperras"][0] += 1;
+                            $_SESSION["tragaperras"][1] += 1;
+                        }
+                        break;
+                    case 2:
+                        $_SESSION["tragaperras"][0] += 4;
+                        $_SESSION["tragaperras"][1] += 1;
+                        break;
+                    case 3:
+                        $_SESSION["tragaperras"][0] += 10;
+                        $_SESSION["tragaperras"][1] += 1;
+                        break;
+                }
+
+                if ($contFresa == 0 && $contIgual > 0 {
+                    $_SESSION["tragaperras"][0] += 2;
+                    $_SESSION["tragaperras"][1] += 1;
+                } else if ($contFresa == 0 && $contIgual > 1) {
+                    $_SESSION["tragaperras"][0] += 5;
+                    $_SESSION["tragaperras"][1] += 1;
+                }
+
+                if ($contFresa == 0 && $contIgual == 0)
+                    $_SESSION["tragaperras"][0] -= 1;
+
+                //$contFresa = 0;
+                //$contIgual = 0;
+            } else {
+                foreach ($_SESSION["frutas"] as $f) {
+                    print("<td> <p style=\"font-size: 600%; margin: 0\"> &#$f </p> </td>");
+                }
             }
-            $cont = 0;
             ?>
             <td>
                 <br /> <br />
--- SERVIDOR/Sesiones(2)/sesiones-2-03B.php
+++ SERVIDOR/Sesiones(2)/sesiones-2-03B.php
@@ -4,7 +4,7 @@ $opc = $_REQUEST["opc"];
 
 if ($opc == "Jugar") {
     if ($_SESSION["tragaperras"][0] > 0) {
-        $_SESSION["tragaperras"][0] -= 1;
+       // $_SESSION["tragaperras"][0] -= 1;
         header("Location: sesiones-2-03A.php?opc=" . $opc);
     } else {
         header("Location: sesiones-2-03A.php");
